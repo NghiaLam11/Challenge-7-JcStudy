d2cee319063629c0811f744c8d8d8c2e36001d5b
hiennghia11 <lamhiennghia1110@gmail.com>
2023-11-06 12:20:05 +0700

    Set up Vite+SCSS+TS+Pinia+VueRouter+ResetCss

---
+++ src/router.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/HomeView.vue"
+const routes = [
+  { path: "/", component: Home },
+];
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+});
+
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        HOME
+    </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
