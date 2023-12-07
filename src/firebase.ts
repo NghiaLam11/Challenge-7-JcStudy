7ae69d420290dd564baa2b2eea95b6ef36179f24
hiennghia11 <lamhiennghia1110@gmail.com>
2023-12-07 11:55:36 +0700

    add firebase hosting

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-tKHNVMny4lDVPPEkjIL6rEpHu88EV8Y",
+  authDomain: "jcstudyy.firebaseapp.com",
+  projectId: "jcstudyy",
+  storageBucket: "jcstudyy.appspot.com",
+  messagingSenderId: "394880466400",
+  appId: "1:394880466400:web:f112e584d512319dab7f85",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
