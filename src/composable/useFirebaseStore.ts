30f0eea7a7dd9240fb31b31df717da6d1db19700
hiennghia11 <lamhiennghia1110@gmail.com>
2023-12-21 12:04:13 +0700

    create blog almost done + res

--- src/composable/useFirebaseStore.ts
+++ src/composable/useFirebaseStore.ts
@@ -1,10 +1,12 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase";
-const useAddUserStore = async (user: any) => {
+export const useAddUserStore = async (user: any) => {
   try {
     const docRef = await addDoc(collection(db, "users"), {});
     console.log("Document written with ID: ", docRef.id);
+    console.log(user);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 };
+
